97b5b7d1e7d6c3b5b6d125083a7c7d0aad0f6258
heqiutong <heqiut@qq.com>
2019-08-01 10:26:05 +0800

    索引接口定义

--- src/main/java/com/github/skyisbule/db/index/Index.java
+++ src/main/java/com/github/skyisbule/db/index/Index.java
@@ -1,4 +1,23 @@
 package com.github.skyisbule.db.index;
 
 public interface Index {
+
+    //将索引同步到文件
+    public boolean sync();
+
+    //从索引文件中初始化内容至内存
+    public void initFromDisk();
+
+    //初始化索引
+    public boolean createIndex();
+
+    //通过主键id拿到记录所属页号
+    public int getPageNum(int id);
+
+    //添加索引
+    public void addIndex(int id);
+
+    //删除索引
+    public void delIndex(int id);
+
 }
--- src/main/java/com/github/skyisbule/db/index/implement/BinarySearch.java
+++ src/main/java/com/github/skyisbule/db/index/implement/BinarySearch.java
@@ -5,4 +5,30 @@ import com.github.skyisbule.db.index.Index;
 public class BinarySearch implements Index {
 
 
+    public boolean sync(){
+
+        return true;
+    }
+
+    public void initFromDisk(){
+
+    }
+
+    public boolean createIndex() {
+        return false;
+    }
+
+    public int getPageNum(int id){
+
+        return 0;
+    }
+
+    public void addIndex(int id){
+
+    }
+
+    public void delIndex(int id){
+
+    }
+
 }
