e41aee060f27017fb30e51dd320689d792726d58
skyisbule <heqiut@qq.com>
2019-08-19 17:46:42 +0800

    实现lru

--- src/main/java/com/github/skyisbule/db/cache/GeneralCache.java
+++ src/main/java/com/github/skyisbule/db/cache/GeneralCache.java
@@ -1,4 +1,19 @@
 package com.github.skyisbule.db.cache;
 
 public class GeneralCache {
+
+    private GeneralCache(){}
+
+    private static volatile GeneralCache generalCache;
+
+    public static GeneralCache getInstance(){
+        if(generalCache == null){
+            synchronized(GeneralCache.class){
+                if (generalCache == null)
+                    generalCache = new GeneralCache();
+            }
+        }
+        return generalCache;
+    }
+
 }
---
+++ src/main/java/com/github/skyisbule/db/cache/LRU1.java
@@ -0,0 +1,68 @@
+package com.github.skyisbule.db.cache;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class LRU1<K, V> {
+    private final int MAX_CACHE_SIZE;
+    private final float DEFAULT_LOAD_FACTORY = 0.75f;
+
+    LinkedHashMap<K, V> map;
+
+    public LRU1(int cacheSize) {
+        MAX_CACHE_SIZE = cacheSize;
+        int capacity = (int)Math.ceil(MAX_CACHE_SIZE / DEFAULT_LOAD_FACTORY) + 1;
+        /*
+         * 第三个参数设置为true，代表linkedlist按访问顺序排序，可作为LRU缓存
+         * 第三个参数设置为false，代表按插入顺序排序，可作为FIFO缓存
+         */
+        map = new LinkedHashMap<K, V>(capacity, DEFAULT_LOAD_FACTORY, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+                return size() > MAX_CACHE_SIZE;
+            }
+        };
+    }
+
+    public synchronized void put(K key, V value) {
+        map.put(key, value);
+    }
+
+    public synchronized V get(K key) {
+        return map.get(key);
+    }
+
+    public synchronized void remove(K key) {
+        map.remove(key);
+    }
+
+    public synchronized Set<Map.Entry<K, V>> getAll() {
+        return map.entrySet();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            stringBuilder.append(String.format("%s: %s  ", entry.getKey(), entry.getValue()));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        LRU1<Integer, Integer> lru1 = new LRU1<>(5);
+        lru1.put(1, 1);
+        lru1.put(2, 2);
+        lru1.put(3, 3);
+        System.out.println(lru1);
+        lru1.get(1);
+        System.out.println(lru1);
+        lru1.put(4, 4);
+        lru1.put(5, 5);
+        lru1.put(6, 6);
+        lru1.get(6);
+        lru1.get(1);
+        System.out.println(lru1);
+    }
+}
