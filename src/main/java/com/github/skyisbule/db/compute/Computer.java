b548c2ea6ccf83e3e53bf521c1c13cdacc102847
jiqiu <qiutong.hqt@alibaba-inc.com>
2019-12-30 15:21:16 +0800

    [feature]定义计算接口

--- src/main/java/com/github/skyisbule/db/compute/Computer.java
+++ src/main/java/com/github/skyisbule/db/compute/Computer.java
@@ -1,14 +1,16 @@
 package com.github.skyisbule.db.compute;
 
+import com.github.skyisbule.db.context.ComputeContext;
+
 /**
  * 计算模型的统一接口
  * 使用者对数据操作的实现都会放在这里
  */
-public interface Computer {
+public interface Computer<T> {
 
-    void init();
+    void init(ComputeContext computeContext);
 
-    void doCompute();
+    void doCompute(T t);
 
     void afterOnePage();
 
