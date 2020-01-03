7d1614f42e020cb7fc1636c61864ff2898f8202b
jiqiu <qiutong.hqt@alibaba-inc.com>
2020-01-03 10:27:46 +0800

    [update]格式化一下代码，让代码更好看点。

--- src/main/java/com/github/skyisbule/db/io/IOCenter.java
+++ src/main/java/com/github/skyisbule/db/io/IOCenter.java
@@ -13,16 +13,16 @@ import java.util.List;
 
 public class IOCenter {
 
-    public boolean writePage(String db, String table, Page page){
+    public boolean writePage(String db, String table, Page page) {
         String path = DefaultConfig.BASE_WORK_PATH + db + "_" + table + ".db";
-        try{
+        try {
             RandomAccessFile file = new RandomAccessFile(path, "rws");
-            int size = 16*1024;
-            file.seek((long)(page.getPageNum()-1)*size);
+            int size = 16 * 1024;
+            file.seek((long)(page.getPageNum() - 1) * size);
             file.write(page.getData());
             file.close();
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return false;
@@ -30,19 +30,22 @@ public class IOCenter {
 
     //todo 这里目前版本预计只实现连续写，即批量写页中页表号表须是连续的 等有空再实现真正的批量写 使其可以跨页表 多次io写
     public boolean writePages(String db, String table, List<Page> pages) {
+        if (pages == null || pages.size() < 1) {
+            return false;
+        }
         String path = DefaultConfig.BASE_WORK_PATH + db + "_" + table + ".db";
-        try{
+        try {
             RandomAccessFile file = new RandomAccessFile(path, "rws");
-            int size = 16*1024*pages.size();
-            file.seek((long)(pages.get(0).getPageNum()-1)*size);
+            int size = 16 * 1024 * pages.size();
+            file.seek((long)(pages.get(0).getPageNum() - 1) * size);
             byte[] bytes = new byte[0];
             for (Page page : pages) {
-                bytes = ByteUtil.byteMerger(bytes,page.getData());
+                bytes = ByteUtil.byteMerger(bytes, page.getData());
             }
             file.write(bytes);
             file.close();
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return true;
@@ -50,46 +53,46 @@ public class IOCenter {
 
     public void createEmptyTable(String db, String table) throws IOException {
         String path = DefaultConfig.BASE_WORK_PATH + db + "_" + table + ".db";
-        File   file = new File(path);
-        if (file.exists()){
+        File file = new File(path);
+        if (file.exists()) {
             boolean delete = file.delete();
-        }else {
-            if (file.createNewFile()){
-                writePage(db,table,buildEmptyPage());
+        } else {
+            if (file.createNewFile()) {
+                writePage(db, table, buildEmptyPage());
             }
         }
     }
 
-    private Page buildEmptyPage(){
+    private Page buildEmptyPage() {
         byte[] pageIdBytes = ByteUtil.int2byte(1);
         byte[] pageIndexId = ByteUtil.int2byte(0);
-        byte[] pageEndPos  = ByteUtil.int2byte(16);
-        byte[] header      = new byte[0];
-        header = ByteUtil.byteMerger(header,pageIdBytes); // page num
-        header = ByteUtil.byteMerger(header,pageIdBytes); // min id
-        header = ByteUtil.byteMerger(header,pageIndexId); // max id
-        header = ByteUtil.byteMerger(header,pageEndPos);  // end pos
+        byte[] pageEndPos = ByteUtil.int2byte(16);
+        byte[] header = new byte[0];
+        header = ByteUtil.byteMerger(header, pageIdBytes); // page num
+        header = ByteUtil.byteMerger(header, pageIdBytes); // min id
+        header = ByteUtil.byteMerger(header, pageIndexId); // max id
+        header = ByteUtil.byteMerger(header, pageEndPos);  // end pos
         //build header end   =>>>>   start build record
-        byte[] data = ByteUtil.byteMerger(header,new byte[1024*16 - header.length]);
-        return new Page(1,1,0,16,data,null);
+        byte[] data = ByteUtil.byteMerger(header, new byte[1024 * 16 - header.length]);
+        return new Page(1, 1, 0, 16, data, null);
     }
 
     public Page getPage(String db, String table, int pageNum) {
         String path = DefaultConfig.BASE_WORK_PATH + db + "_" + table + ".db";
         Page page = null;
-        try{
+        try {
             RandomAccessFile file = new RandomAccessFile(path, "r");
-            int size = 16*1024;
-            if (file.length() < 1) return buildEmptyPage();
+            int size = 16 * 1024;
+            if (file.length() < 1) { return buildEmptyPage(); }
             byte[] data = new byte[size];
-            file.seek((pageNum-1)*size);
+            file.seek((pageNum - 1) * size);
             file.read(data);
             file.close();
-            int minId      = ByteUtil.byte2int(ByteUtil.cut(data,4,4));
-            int maxId      = ByteUtil.byte2int(ByteUtil.cut(data,8,4));
-            int pageEndPos = ByteUtil.byte2int(ByteUtil.cut(data,12,4));
-            page = new Page(pageNum,minId,maxId,pageEndPos,data,null);
-        }catch (Exception e){
+            int minId = ByteUtil.byte2int(ByteUtil.cut(data, 4, 4));
+            int maxId = ByteUtil.byte2int(ByteUtil.cut(data, 8, 4));
+            int pageEndPos = ByteUtil.byte2int(ByteUtil.cut(data, 12, 4));
+            page = new Page(pageNum, minId, maxId, pageEndPos, data, null);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return page;
@@ -97,15 +100,14 @@ public class IOCenter {
 
     public List<Page> getPages(String db, String table, int pageBegin, int pageEnd) {
 
-
         return null;
     }
 
-    public List<String> getDBs(){
+    public List<String> getDBs() {
         List<String> dbs = new ArrayList<>();
         File folder = new File(DefaultConfig.BASE_WORK_PATH);
         File[] files = folder.listFiles();
-        if (files != null){
+        if (files != null) {
             Arrays.stream(files)
                 .filter(file -> file.getAbsolutePath().endsWith(".db"))
                 .forEach(file -> dbs.add(file.getName()));
@@ -113,12 +115,12 @@ public class IOCenter {
         return dbs;
     }
 
-    public boolean drop(String db){
-        if (db == null){
+    public boolean drop(String db) {
+        if (db == null) {
             return true;
         }
         File file = new File(db);
-        if (file.exists() && file.isFile()){
+        if (file.exists() && file.isFile()) {
             return file.delete();
         }
         return false;
